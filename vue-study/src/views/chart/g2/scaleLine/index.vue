3eb9517e7c79089c580204ed19e458db26b01a9f
yeyue03 <632491231@qq.com>
2023-06-13 17:20:40 +0800

    feat: vue - Antv G2 - 设置度量scale 中的min、max 刻度数组起始、终止刻度（时间戳），达到缩放效果; 超出刻度的数据截掉

--- vue-study/src/views/chart/g2/scaleLine/index.vue
+++ vue-study/src/views/chart/g2/scaleLine/index.vue
@@ -27,31 +27,36 @@ export default defineComponent({
     const defaultFormat = 'YYYY-MM-DD hh:mm'
     let charData = [];
     let dateTicks = [];
+    let minTime = 0;
+    let maxTime = 0;
+    let tickCount = 4;
 
     const setChartData = () => {
       let _arr = [];
       let timestamp = new Date().getTime();
 
-      let prevTime = timestamp - 30 * 60 * 1000;
-      dateTicks.push(prevTime)
+      minTime = timestamp;
+      dateTicks.push(minTime)
 
       for (let i=0; i<10; i++) {
         timestamp += 10 * 60 * 1000;
         dateTicks.push(timestamp);
         // const _date = dayjs(timestamp).format(defaultFormat);
+        let beam = getRandom();
+        if (i>3 && i<7) {
+          beam = undefined
+        }
         _arr.push({
           date: timestamp,
           city: '北京',
-          temperature: getRandom()
-        }, {
-          date: timestamp,
-          city: '成都',
-          temperature: getRandom()
+          temperature: getRandom(),
+          humidity: getRandom(),
+          beam
         })
       }
 
-      let nextTime = timestamp + 30 * 60 * 1000;
-      dateTicks.push(nextTime)
+      maxTime = _arr[_arr.length - 1].date;
+      dateTicks.push(maxTime)
 
       charData = _arr;
     }
@@ -78,7 +83,9 @@ export default defineComponent({
       chart.scale({
         date: {
           range: [0, 1],
-          ticks: dateTicks
+          // min: minTime,
+          // max: maxTime,
+          // ticks: dateTicks
           // ticks: ['2023-06-13 03:30', '2023-06-13 03:40', '2023-06-13 03:50', '2023-06-13 04:00', '2023-06-13 04:30', '2023-06-13 05:30', '2023-06-13 06:30'],
         },
         temperature: {
@@ -86,13 +93,19 @@ export default defineComponent({
           nice: true,
           min: 0,
           max: 100,
-          tickCount: 10, // 坐标轴上刻度点的个数
+          // tickCount: 10, // 坐标轴上刻度点的个数
         },
       });
 
       chart.tooltip({
         showCrosshairs: true,
         shared: true,
+        title: (val) => {
+          return dayjs(Number(val)).format(defaultFormat);
+        },
+        formatter: (val) => {
+          return dayjs(Number(val)).format(defaultFormat);
+        },
       });
 
       chart.axis('date', {
@@ -135,19 +148,14 @@ export default defineComponent({
         },
       });
 
-      chart
-        .line()
-        .position('date*temperature')
-        .color('city')
-        // .shape('smooth')
-        .size(3);
+      chart.line().position('date*temperature').size(3).color('#f00');
+      chart.point().position('date*temperature').color('city').shape('circle').size(3);
 
-      chart
-        .point()
-        .position('date*temperature')
-        .color('city')
-        .shape('circle')
-        .size(5);
+      chart.line().position('date*humidity').size(3).color('#0f0');
+      chart.point().position('date*humidity').color('city').shape('circle').size(3);
+
+      chart.line().position('date*beam').size(3).color('#00f');
+      chart.point().position('date*beam').color('city').shape('circle').size(3);
 
       chart.render();
     };
@@ -168,15 +176,54 @@ export default defineComponent({
       scaleObj.width = chartWrapBoxRef.value.clientWidth;
 
       if (type == "amplify") { // 放大
+        minTime = minTime - 10 * 60 * 1000;
+        maxTime = maxTime + 10 * 60 * 1000;
+        tickCount++;
         scaleObj.scale++;
       } else if (type == "reduce") { // 缩小
+        minTime = minTime + 10 * 60 * 1000;
+        maxTime = maxTime - 10 * 60 * 1000;
+        tickCount--;
         scaleObj.scale--;
       } else if (type == "restore") { // 还原
+        minTime = charData[0].date;
+        maxTime = charData[charData.length - 1].date;
         scaleObj.scale = 0;
+        tickCount = 4;
+      }
+
+      if (tickCount <= 2) {
+        tickCount = 2;
       }
 
       scaleObj.height = initHeight + scaleObj.scale * 50;
+
+      chart.scale({
+        date: {
+          range: [0, 1],
+          min: minTime,
+          max: maxTime,
+          // ticks: dateTicks
+          // ticks: ['2023-06-13 03:30', '2023-06-13 03:40', '2023-06-13 03:50', '2023-06-13 04:00', '2023-06-13 04:30', '2023-06-13 05:30', '2023-06-13 06:30'],
+        },
+        temperature: {
+          alias: '温度',
+          type: 'linear',
+          nice: true,
+          min: 0,
+          max: 100,
+          tickCount: tickCount, // 坐标轴上刻度点的个数
+        },
+      });
+      
       chart.changeSize(scaleObj.width, scaleObj.height);
+
+      console.log("=== minTime, maxTime: ", minTime, maxTime);
+      const orgData = JSON.parse(JSON.stringify(charData));
+      const needData = orgData.filter(item => {
+        return item.date >= minTime && item.date <= maxTime
+      });
+      chart.changeData(needData);
     }
 
     onMounted(() => {
